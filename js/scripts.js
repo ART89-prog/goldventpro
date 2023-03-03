4c1587baeedea0b2c784777ebda3dba302fb8e38
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2023-03-03 15:46:30 +0300

    567754675467

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,30 @@ $(() => {
   })
 
 
+  const swiper = new Swiper('.swiper-container', {
+    // Default parameters
+    slidesPerView: 2,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
+  })
+
 
   // Аккордион
   $('body').on('click', '.accordion .accordion_item .accordion_item-head', function (e) {
